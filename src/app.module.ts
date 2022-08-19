b3ea740869c6111de9f5572b7167e20df39eff54
Tomoyuki Toda <tomoyuki.toda@fork.co.jp>
2022-08-19 17:59:43 +0900

    初期設定を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { SurveyModule } from './survey/survey.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SurveyModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        useLevel: `${process.env.NODE_ENV}` !== 'production' ? 'info' : 'info',
+        timestamp: () =>
+          `,"time":"${new Date().toLocaleString('ja-JP', {
+            timeZone: 'Asia/Tokyo',
+          })}"`,
+        formatters: {
+          level: (label) => {
+            return { level: label };
+          },
+          bindings: (bindings) => {
+            return {
+              env: `${process.env.NODE_ENV}`,
+              // pid: bindings.pid,
+              // hostname: bindings.hostname,
+            };
+          },
+          log: (object) => {
+            return object;
+          },
+        },
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from 'nestjs-pino';
+import path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    { bufferLogs: true },
+  );
+  app.useLogger(app.get(Logger));
+
+  app.useStaticAssets({
+    root: path.resolve(__dirname, '..', 'public'),
+  });
+  app.setViewEngine({
+    engine: {
+      handlebars: require('hbs'),
+    },
+    templates: path.resolve(__dirname, '..', 'views'),
+  });
+
   await app.listen(3000);
 }
 bootstrap();
